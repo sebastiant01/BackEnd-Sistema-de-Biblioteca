9f20ebd559ce5e856a20a9576a3f90463f3a2578
LechiHound69 <cristobalmejiam@gmail.com>
2026-02-10 13:33:00 -0500

    feat: clase usuario creada

---
+++ clases/Usuario.py
@@ -0,0 +1,21 @@
+class Usuario:
+    """
+    Representa un usuario, con nombre y documento.
+
+    Attributes:
+        nombre (str): Nombre del usuario.
+        DNI (int): DNI del usuario.
+        libros_prestados (list): Lista de libros que actualmente tiene el usuario prestados.
+    """
+
+    def __init__(self, nombre: str, DNI: int, libros_prestados: list):
+        self.nombre = nombre
+        self.DNI = DNI
+        self.libros_prestados = libros_prestados
+        # luego vemos qué más le ponemos al usuario, primero necesitaríamos desarrollar
+        # en equipo la lógica y flujo que vamos a seguir
+
+    def mostrar_data(self):
+        print(
+            f"Nombre: {self.nombre}, DNI: {self.DNI}, Libros prestados actualmente: {self.libros_prestados}"
+        )
