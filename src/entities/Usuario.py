3e700a5e1ac8a5a7460d6d4d8a7adf4fe2888db5
LechiHound69 <cristobalmejiam@gmail.com>
2026-02-23 23:59:43 -0500

    refactor: cambié importaciones, borré atributos innecesarios, modifiqué los métodos, borré el try except y moví el menú a la clase main

--- src/entities/Usuario.py
+++ src/entities/Usuario.py
@@ -1,6 +1,6 @@
 import abc
-import hashlib
-from Libro import Libro
+from src.entities.Biblioteca import Biblioteca
+from src.entities.MaterialBiblioteca import MaterialBiblioteca
 
 
 class Usuario(abc.ABC):
@@ -14,36 +14,16 @@ class Usuario(abc.ABC):
         contrasena (str): Contraseña del usuario.
     """
 
-    def __init__(self, nombre: str, DNI: str, usuario: str, contrasena: str):
+    def __init__(self, nombre: str, DNI: str):
         """
         Inicializa una nueva instancia de Biblioteca.
 
         Args:
             nombre (str): Nombre del usuario.
             DNI (str): DNI del usuario.
-            usuario (str): Usuario de inicio de sesión.
-            contrasena (str): Contraseña del usuario.
         """
         self.nombre = nombre
         self.DNI = DNI
-        self.usuario = usuario
-        self.contrasena = contrasena
-
-    @abc.abstractmethod
-    def mostrar_menu(self):
-        """
-        Método abstracto que muestra el menú de opciones, el cual es diferente para el cliente
-        que para el empleado.
-
-        Cada menú contiene como opciones las acciones que puede hacer cada usuario
-        dependiendo de su tipo
-
-        Returns:
-            None
-        """
-        print(
-            f"Nombre: {self.nombre}, DNI: {self.DNI}, Libros prestados actualmente: {self.libros_prestados}"
-        )
 
     @abc.abstractmethod
     def mostrar_data(self):
@@ -53,9 +33,6 @@ class Usuario(abc.ABC):
         Returns:
             None
         """
-        print(
-            f"Nombre: {self.nombre}, DNI: {self.DNI}, Libros prestados actualmente: {self.libros_prestados}"
-        )
 
 
 class Cliente(Usuario):
@@ -68,109 +45,72 @@ class Cliente(Usuario):
         DNI (str): DNI del usuario. (Heredado)
         usuario (str): Usuario de inicio de sesión. (Heredado)
         contrasena (str): Contraseña del usuario. (Heredado)
-        libros_prestados (list): Libros que el usuario tiene prestados. Es una lista de
-            instancias de la clase 'Libro'
+        material_prestado (str): Material que el usuario tiene prestado. Es una lista de
+            strings, cada elemento siendo el código de un material bibliográfico
     """
 
-    def __init__(
-        self,
-        nombre: str,
-        DNI: str,
-        usuario: str,
-        contrasena: str,
-        libros_prestados: list[Libro],
-    ):
+    def __init__(self, nombre: str, DNI: str, material_prestado: list[str]):
         """
         Inicializa una nueva instancia de un usuario de tipo cliente.
 
         Args:
             nombre (str): Nombre del cliente.
             DNI (str): DNI del cliente.
-            usuario (str): Usuario de inicio de sesión.
-            contrasena (str): Contraseña del cliente.
-            libros_prestados (list): Lista de libros prestados por el cliente actualmente.
+            material_prestado (str): Lista de materiales prestados por el cliente actualmente.
         """
-        super().__init__(nombre, DNI, usuario, contrasena)
-        self.libros_prestados = libros_prestados
+        super().__init__(nombre, DNI)
+        self.material_prestado = material_prestado
 
-    def mostrar_menu(self):
+    def devolver_libro(self) -> None:
         """
-        Método que muestra el menú de opciones que el cliente puede hacer.
-
-        Opciones:
-            - Consultar libro: Permite buscar si un libro existe en la biblioteca
-            - Prestar libro: Permite pedir prestado un libro si hay unidades disponibles
-            - Devolver libro: Permite devolver un libro que el cliente pidió prestado
-            - Ver datos de usuario: Permite ver los datos del cliente activo
-            - Cerrar sesión: Cierra sesión de forma segura y finaliza el programa
-
-        Returns:
-            None
-        """
-        while True:
-            print(f"--- Menú de usuario ---\n Bienvenido, {self.nombre}.")
-            print(
-                "Opción 1: Consultar libro, revista, etc.\n"
-                "Opción 2: Prestar libro, revista, etc.\n"
-                "Opción 3: Devolver libro.\n"
-                "Opción 4: Ver datos de usuario.\n"
-                "Opción 5: Cerrar sesión"
-            )
-            opcion = input("Ingrese una opción: ")
-            match opcion:
-                case "1":
-                    print("Ingresó 'Consultar libro, revista, etc'\n")
-                    # aquí iría la llamada al método de consultar libro xdxd
-                case "2":
-                    print("Ingresó 'Prestar libro, revista, etc.'\n")
-                    # aquí iría la llamada al método de prestar libro XD
-                case "3":
-                    print("Ingresó 'Devolver libro'\n")
-                    self.devolver_libro()
-                case "4":
-                    print("Ingresó 'Ver datos de usuario'\n")
-                    super().mostrar_data()
-                case "5":
-                    print("Cerrando sesión...\n")
-                    break
-                case _:
-                    print("Opción inválida. Por favor intente otra vez.\n")
-
-        return "Cerrando sesión..."
-
-    def devolver_libro(self):
-        """
-        Método que le permite al cliente devolver un libro de su lista de libros prestados.
+        Método que le permite al cliente devolver un material de su lista de materiales prestados.
 
         Returns:
             None
         """
         while True:
             n = 1
-            for libro in self.libros_prestados:
-                print(f"Opción {n}: {libro.titulo}, {libro.autor}")
-                n += 1
-            opcion = int(input("Ingrese una opción: "))
-            try:
-                lib = self.libros_prestados[opcion - 1]
-                lib.disponible = True
-                lib.unidades += 1
-                del self.libros_prestados[opcion - 1]
-            except Exception:
-                print("Por favor ingrese una opción válida.")
-                continue
-            print(f"El libro '{lib.titulo}' fue devuelto con éxito")
-            break
+            size = len(self.material_prestado)
+            for codigo in self.material_prestado:
+                libro = Biblioteca.buscar_por_codigo(codigo)
+                if isinstance(libro, MaterialBiblioteca):
+                    print(f"Opción {n}: {libro.titulo}, {libro.autor}")
+                    n += 1
+                else:
+                    continue
+
+            opcion = input("Ingrese una opción: ").strip()
+            if opcion.isdigit():
+                opcion = int(opcion)
+
+                if 1 <= opcion <= size:
+                    codigo = self.material_prestado[opcion - 1]
+                    material = Biblioteca.buscar_por_codigo(codigo)
+                    if isinstance(material, MaterialBiblioteca):
+                        material.disponible = True
+                        material.unidades += 1
+                        del self.material_prestado[opcion - 1]
+                        print("Devuelto con éxito!")
+                        break
+                    else:
+                        print("Error: No se pudo devolver")
+                        continue
+                else:
+                    print("Por favor ingrese una opción válida.")
+                    continue
+
         return
 
-    def mostrar_data(self):
+    def mostrar_data(self) -> None:
         """
         Método que imprime los datos del cliente activo.
 
         Returns:
             None
         """
-        return super().mostrar_data()
+        print(
+            f"Nombre: {self.nombre}, DNI: {self.DNI}, Libros prestados actualmente: {self.material_prestado}"
+        )
 
 
 class Empleado(Usuario):
@@ -185,73 +125,21 @@ class Empleado(Usuario):
         contrasena (str): Contraseña del empleado.
     """
 
-    def __init__(self, nombre, DNI, usuario, contrasena):
+    def __init__(self, nombre, DNI) -> None:
         """
         Inicializa una nueva instancia de un usuario de tipo empleado.
 
         Args:
             nombre (str): Nombre del empleado.
             DNI (str): DNI del empleado.
-            usuario (str): Usuario de inicio de sesión.
-            contrasena (str): Contraseña del empleado.
         """
-        super().__init__(nombre, DNI, usuario, contrasena)
+        super().__init__(nombre, DNI)
 
-    def mostrar_menu(self):
-        """
-        Método que muestra el menú de opciones que el cliente puede hacer.
-
-        Opciones:
-            - Consultar libro: Permite buscar si un libro existe en la biblioteca
-            - Crear usuario: Permite crear un usuario nuevo
-            - Eliminar usuario: Permite eliminar un usuario del sistema
-            - Eliminar libro: Permite eliminar un libro del catálogo
-            - Ver datos de usuario: Permite ver los datos del empleado activo
-            - Cerrar sesión: Cierra sesión de forma segura y finaliza el programa
-
-        Returns:
-            None
-        """
-        while True:
-            print(f"--- Menú de usuario ---\n Bienvenido, {self.nombre}.")
-            print(
-                "Opción 1: Consultar libro, revista, etc.\n"
-                "Opción 2: Crear usuario\n"
-                "Opción 3: Eliminar usuario\n"
-                "Opción 4: Eliminar libro\n"
-                "Opción 5: Ver datos de usuario.\n"
-                "Opción 6: Cerrar sesión"
-            )
-            opcion = input("Ingrese una opción: ")
-            match opcion:
-                case "1":
-                    print("Ingresó 'Consultar libro, revista, etc'\n")
-                    # aquí iría la llamada al método de consultar libro
-                case "2":
-                    print("Ingresó 'Crear usuario'\n")
-                    # aquí iría la llamada al método de crear usuario uwu
-                case "3":
-                    print("Ingresó 'Eliminar usuario'\n")
-                    # aquí irá la llamada al método de eliminar usuario
-                case "4":
-                    print("Ingresó 'Eliminar libro'\n")
-                    # aquí irá la llamada al método de eliminar libro Bv
-                case "5":
-                    print("Ingresó 'Ver datos de usuario'\n")
-                    super().mostrar_data()
-                case "6":
-                    print("Cerrando sesión...\n")
-                    break
-                case _:
-                    print("Opción inválida. Por favor intente otra vez.\n")
-
-        return "Cerrando sesión..."
-
-    def mostrar_data(self):
+    def mostrar_data(self) -> None:
         """
         Método que imprime los datos del empleado activo.
 
         Returns:
             None
         """
-        return super().mostrar_data()
+        print(f"Nombre: {self.nombre}, DNI: {self.DNI}")
