4d78e8dd98d70d445c13a5a28ed19167273735b9
Cris Mejia <140118075+LechiHound69@users.noreply.github.com>
2026-03-15 15:59:23 -0500

    refactor: ajustes en relaciones de la clase Reserva

--- src/entities/Reserva.py
+++ src/entities/Reserva.py
@@ -6,7 +6,9 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from src.database.config import Base
-from src.entities import Auditoria
+from src.entities.Auditoria import Auditoria
+from src.entities.Usuario import Usuario
+from src.entities.MaterialBiblioteca import MaterialBiblioteca
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
@@ -19,12 +21,12 @@ class Reserva(Base, Auditoria):
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
     )
     id_usuario = Column(UUID(as_uuid=True), ForeignKey("Usuario.id_usuario"))
-    usuario_reserva = relationship("Usuario", back_populates="reservas")
+    usuario_reserva = relationship("Usuario", back_populates="reserva")
 
     id_material = Column(
         UUID(as_uuid=True), ForeignKey("MaterialBiblioteca.id_material")
     )
-    material_reservado = relationship("MaterialBiblioteca", back_populates="reservas")
+    material_reservado = relationship("MaterialBiblioteca", back_populates="reserva")
 
     fecha_reserva = Column(DateTime(timezone=True), server_default=func.now())
     estado = Column(String, nullable=False, default="Activa")
