ba5dcaad700785b9718d3fdc376b32d9cbddd754
Cris Mejia <140118075+LechiHound69@users.noreply.github.com>
2026-03-16 16:35:18 -0500

    feat: clase autor añadida

---
+++ src/entities/Autor.py
@@ -0,0 +1,22 @@
+import uuid
+import os
+import sys
+from sqlalchemy import Column, String, CheckConstraint, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from src.database.config import Base
+from src.entities.Auditoria import Auditoria
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
+class Autor(Base, Auditoria):
+
+  id_autor = Column(
+    UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+  )
+  nombre_autor = Column(String(50), nullable=False)
+  apellido_autor = Column(String(50), nullable=True)
+  nacionalidad = Column(String(50), nullable=True)
+  
